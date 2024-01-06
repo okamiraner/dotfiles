a523a82c04eeb709d59ac2aa777a4f465ed627f1
okamiraner <c.mararenko@gmail.com>
2024-01-07 00:45:30 +0300

    upd

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,4 @@
-
-local lsp = require('lsp-zero')
+local lsp_zero = require('lsp-zero')
 local lspconfig = require('lspconfig')
 
 
@@ -11,64 +10,69 @@ require('mason-lspconfig').setup({
         'ltex'
     },
     handlers = {
-        lsp.default_setup,
+        lsp_zero.default_setup,
         lua_ls = function()
-            local lua_opts = lsp.nvim_lua_ls()
+            local lua_opts = lsp_zero.nvim_lua_ls()
             lspconfig.lua_ls.setup(lua_opts)
         end,
     }
 })
+
 
 local cmp = require('cmp')
 local cmp_select = {
     behavior = cmp.SelectBehavior.Select
 }
 
+
 cmp.setup({
     sources = {
-        {name = 'path'},
-        {name = 'nvim_lsp'},
-        {name = 'nvim_lua'},
+        {
+            name = 'path'
+        },
+        {
+            name = 'nvim_lsp'
+        },
+        {
+            name = 'nvim_lua'
+        },
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-        ['<C-Space>'] = cmp.mapping.complete(),
-    }),
+        ['<CR>'] = cmp.mapping.confirm({select = true}),
+        ['<leader><tab>'] = cmp.mapping.complete(),
+    })
 })
 
-
-lsp.set_sign_icons({
-    error = 'E',
-    warn = 'W',
-    hint = 'H',
-    info = 'I'
-})
 
 vim.diagnostic.config({
-    virtual_text = {
-        severity_limit = 'Error'
-    },
-    signs = {
-        severity_limit = 'all'
-    },
-    underline = {
-        severity_limit = 'Error'
-    },
+    virtual_text = false,
     update_in_insert = true
 })
 
 
-lsp.on_attach(function(client, bufnr)
-    lsp.default_keymaps({buffer = bufnr})
+lsp_zero.on_attach(function(client, bufnr)
+    lsp_zero.default_keymaps({
+        buffer = bufnr
+    })
+
     local opts = {buffer = bufnr}
 
+    vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set('n', '<leader>dd', '<cmd>Telescope diagnostics bufnr=0<cr>', opts)
+    vim.keymap.set('n', '<leader>d', 'vim.lsp.diagnostic.show_line_diagnostics()', opts)
     vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', opts)
     vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<cr>', opts)
     vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
 end)
+
+
+lsp_zero.set_sign_icons({
+  error = '♢',
+  warn = '♢',
+  hint = '♢',
+  info = '♢',
+})
+
 
 lspconfig.ltex.setup({
     filetypes = {
