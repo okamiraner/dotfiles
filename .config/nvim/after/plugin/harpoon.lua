b652e4c5eece8676afc4a109cb5451eead881da4
okamiraner <c.mararenko@gmail.com>
2024-01-08 10:45:26 +0300

    upd

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,13 +1,16 @@
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
+local harpoon = require('harpoon')
 
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
 
-vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
-vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end)
-vim.keymap.set("n", "<leader>6", function() ui.nav_file(6) end)
-vim.keymap.set("n", "<leader>7", function() ui.nav_file(7) end)
+harpoon:setup()
+
+vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
+vim.keymap.set('n', '<leader>h', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set('n', '<leader>1>', function() harpoon:list():select(1) end)
+vim.keymap.set('n', '<leader>2', function() harpoon:list():select(2) end)
+vim.keymap.set('n', '<leader>3', function() harpoon:list():select(3) end)
+vim.keymap.set('n', '<leader>4', function() harpoon:list():select(4) end)
+
+vim.keymap.set('n', '<leader>}', function() harpoon:list():prev() end)
+vim.keymap.set('n', '<leader>{', function() harpoon:list():next() end)
+
